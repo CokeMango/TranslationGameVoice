1d0a3256e9e55245cbedfe228f7e6258fbfa4c35
CokeMango <93448780+CokeMango@users.noreply.github.com>
2023-03-01 22:33:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+click_to_convert.addEventListener('click', function(){
+    
+    var speech = true;
+    var speech = true;
+    window.SpeechRecognition = window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+    recognition.lang = 'es-US';
+
+    recognition.addEventListener('result', e=> {
+        const transcript = Array.from(e.results)
+        .map(result =>result[0])
+        .map(result => result.transcript)
+
+        
+        convert_text.innerHTML = transcript;
+        console.log(transcript);
+        
+    })
+
+
+
+    
+    if(speech == true){
+        recognition.start();
+
+    }
+
+
+})
+
+
+
+
